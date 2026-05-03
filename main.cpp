d725d3cb5076f78948c9f84c29a3e56be3934db2
alijunaidaaa-coder <alijunaid.aaa@gmail.com>
2026-05-03 17:50:23 +0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     cout<<"Hello world"<<endl;
+    cout << "i am ali junaid and doing my first commit change here"<<endl;
     sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
