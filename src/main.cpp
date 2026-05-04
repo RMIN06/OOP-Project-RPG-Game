fe6405091810f09bf875fe252008ab83c6245708
chaudharyhassaan <hassaanchaudhary2007@gmail.com>
2026-05-04 23:18:33 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,8 @@
-#include <SFML/Graphics.hpp>
 #include<iostream>
 using namespace std;
 int main()
 {
-    cout<<"Hello world"<<endl;
-    cout << "i am ali junaid and doing my first commit change here"<<endl;
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    cout<<"Hello"<<endl;
+    return 0;
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
 }
